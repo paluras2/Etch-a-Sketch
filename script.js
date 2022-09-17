cd2168a06088bb52e488308b4e50d0a957c06e36
Palura Andrei <paluraa@yahoo.com>
2022-09-17 16:52:06 +0300

    Did CSS and change color event

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 
 const container = document.querySelector('.cont');
-container.style.display = "flex";
-container.style.padding = "100px 100px"
+container.style.padding = "11px"
+container.style.display = "grid"
+container.style.gridTemplateColumns= `repeat(16,10px)`
+container.style.border= "1px solid #A1C298"
+container.style.content="auto"
 
 
 
 
 
 
-for(i = 0 ; i<16;i++){
+
+
+
+
+
+
+
+
+
+
+for(i = 0 ; i<16*16;i++){
     const newDiv = document.createElement("div");
     newDiv.className = "theDiv";
-    newDiv.style.padding = "20px";
-    newDiv.style.backgroundColor = "black"
-    newDiv.style.border = "1px solid red"
+    newDiv.style.padding="5px 5px"
+    newDiv.style.backgroundColor = "#C6EBC5"
+    newDiv.style.border = "1px solid #FBF2CF"
 
     document.querySelector(".cont").appendChild(newDiv)
+
+    newDiv.onmouseover = function changeColor(x){
+        newDiv.style.backgroundColor="black"
+    
+    }
+    
+
 }
+
+
 
 
 
